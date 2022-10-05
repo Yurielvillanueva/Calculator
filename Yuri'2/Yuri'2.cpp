3762e753d46e0ab93b8d0ef800036e0a0659d502
Yurielvillanueva <107783416+Yurielvillanueva@users.noreply.github.com>
2022-10-05 17:20:41 +0800

    Add project files.

---
+++ Yuri'2/Yuri'2.cpp
@@ -0,0 +1,27 @@
+//Calculator
+
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	int numOne;
+	int numTwo;
+
+	cout << "Enter Number: ";
+	cin >> numOne;
+
+	cout << "Enter Another Number: ";
+	cin >> numTwo;
+
+	cout << numOne * numTwo;
+
+	return 0;
+
+}
+
+
+
+
